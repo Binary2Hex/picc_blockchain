614919e4d59c12b2cd3e7aef724de93c8effcb74
Xi Xue Jia <xixuejia@cn.ibm.com>
2016-11-07 17:52:45 +0800

    init protobuf for farm

--- chaincode/picc/farm/src/farm/farm.go
+++ chaincode/picc/farm/src/farm/farm.go
@@ -2,22 +2,26 @@ package main
 
 import "fmt"
 import "github.com/hyperledger/fabric/core/chaincode/shim"
+import "github.com/op/go-logging"
+
+var ccLogger = logging.MustGetLogger("FarmCC")
 
 type FarmCC struct {
 }
 
 func (t *FarmCC) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-	fmt.Println("init called")
+	ccLogger.Debug("Init called!")
+
 	return nil, nil
 }
 
 func (t *FarmCC) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-	fmt.Println("invoke called")
+	ccLogger.Debug("Invoke called!")
 	return nil, nil
 }
 
 func (t *FarmCC) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-	fmt.Println("query called")
+	ccLogger.Debug("Query called!")
 	return nil, nil
 }
 
@@ -26,4 +30,8 @@ func main() {
 	if err != nil {
 		fmt.Printf("Error starting FarmCC : %s", err)
 	}
+}
+
+func (t *FarmCC) createFarmTable(stub *shim.ChaincodeStub) error {
+	return nil
 }
